3a0f49db1d5bb2c0a51fba207b9267ce2a7ad3a6
Bojan Vrhovnik <58401850+bovrhovn@users.noreply.github.com>
2022-12-05 10:43:43 +0100

    adding simple powershell implementation to demonstrate usage

---
+++ src/ISDemosSLN/K8SCmdlet/GetPodsCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Management.Automation;
+using k8s;
+
+namespace K8SCmdlet;
+
+[Cmdlet(VerbsCommon.Get, "Pods")]
+public class GetPodsCommand : Cmdlet
+{
+    [Parameter(Position = 0, ValueFromPipeline = true,
+        ValueFromPipelineByPropertyName = true)]
+    [ValidateNotNullOrEmpty]
+    public string? NamespaceName { get; set; }
+
+    protected override void ProcessRecord()
+    {
+        var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+        IKubernetes client = new Kubernetes(config);
+        
+        if (string.IsNullOrEmpty(NamespaceName)) NamespaceName = "default";
+        
+        var podList = client.CoreV1.ListNamespacedPod(NamespaceName);
+
+        foreach (var currentPod in podList.Items)
+        {
+            WriteObject(currentPod, true);
+        }
+
+        base.ProcessRecord();
+    }
+}
